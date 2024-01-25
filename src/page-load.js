c09e764fd4afaf4483bbacd4a5a535a747804a4e
Draven <valdevisor@gmail.com>
2024-01-26 05:56:08 +0800

    Initial Commit

--- src/page-load.js
+++ src/page-load.js
@@ -1,16 +1,10 @@
 "use strict";
 
-// import createTodo from "./create-to-do";
-import uiModule from "./uiModule";
-// import localStorageModule from "./localStorageModule"
-import displayBooks from "./displayModule";
+import createTodo from "./create-to-do";
 
 function pageLoad() {
   const content = document.getElementById("content");
-  // createTodo();
-  uiModule();
-  displayBooks();
-  // localStorageModule()
+  createTodo();
 }
 
 export default pageLoad;
