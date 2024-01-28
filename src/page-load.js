f95f1aa6f2461734d191289b865b1510c785bca6
Draven <valdevisor@gmail.com>
2024-01-28 23:20:45 +0800

    modified page-load

--- src/page-load.js
+++ src/page-load.js
@@ -1,9 +1,13 @@
 "use strict";
 
 import createTodo from "./create-to-do";
+import createHeader from "./header";
+import createAddList from "./addLIst";
 
 function pageLoad() {
   const content = document.getElementById("content");
+  createHeader();
+  createAddList();
   createTodo();
 }
 
