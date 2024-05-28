1a4c237bbbc688650be8214b138139d3aaef319d
Draven <valdevisor@gmail.com>
2024-05-28 19:27:14 +0800

    update comments

--- src/page-load.js
+++ src/page-load.js
@@ -6,7 +6,7 @@
 import createTodo from "./create-to-do";
 import createHeader from "./header";
 import createAddList from "./addList";
-S;
+
 function pageLoad() {
   createHeader();
   createAddList();
