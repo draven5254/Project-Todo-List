17b180ff2f74b5dc3b734f823de7ce5c2918518b
Draven <valdevisor@gmail.com>
2024-07-24 18:45:12 +0800

    update comments

--- src/page-load.js
+++ src/page-load.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Page Load Website
-// this is for github update 90
+// this is for github update 91
 
 import createTodo from "./create-to-do";
 import createHeader from "./header";
